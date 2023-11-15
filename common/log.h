ac9b70de00b8d80ad952ed1c8fe2fa323e8ad8d8
Jesús (gsus) Lapastora <cyber.gsuscode@gmail.com>
2023-11-15 22:22:01 +0100

    common: Make logs output file & line

--- common/log.h
+++ common/log.h
@@ -1,7 +1,15 @@
 #pragma once
 #include <stdio.h>
 
-#define warn(msg, ...) fprintf(stderr, "(warn) " msg __VA_OPT__(, ) __VA_ARGS__)
+#define _S(x) #x
+#define S(x) _S(x)
+
+#define warn(msg, ...)                                                         \
+    fprintf(stderr, "\x1b[1m" __FILE_NAME__ "\x1b[m:" S(                            \
+                        __LINE__) ":\twarn: " msg __VA_OPT__(, ) __VA_ARGS__)
 #define error(msg, ...)                                                        \
-    fprintf(stderr, "(error) " msg __VA_OPT__(, ) __VA_ARGS__)
-#define log(msg, ...) fprintf(stderr, "(info) " msg __VA_OPT__(, ) __VA_ARGS__)
+    fprintf(stderr, "\x1b[1m" __FILE_NAME__ "\x1b[m:" S(                            \
+                        __LINE__) ":\terror: " msg __VA_OPT__(, ) __VA_ARGS__)
+#define log(msg, ...)                                                          \
+    fprintf(stderr, "\x1b[1m" __FILE_NAME__ "\x1b[m:" S(                       \
+                        __LINE__) ":\tinfo: " msg __VA_OPT__(, ) __VA_ARGS__)
