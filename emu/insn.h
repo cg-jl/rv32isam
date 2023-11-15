c15129be5c4390d8c116418834baec214462e459
Jesús (gsus) Lapastora <cyber.gsuscode@gmail.com>
2023-11-15 20:34:24 +0100

    emu: Write out all RISC-V instructions

--- emu/insn.h
+++ emu/insn.h
@@ -2,45 +2,242 @@
 // Created by cg on 10/17/23.
 //
 
-#ifndef SAM_INSN_H
-#define SAM_INSN_H
+#pragma once
 
-#include <stdint.h>
+#include "../common/types.h"
 
-enum insn_code {
-    INSN_LUI = 0b0110111,
-    INSN_AUIPC = 0b0010111,
-    INSN_ADDI_SLTI = 0b0010011,
+// RISC-V Specification:
+// https://riscv.org/wp-content/uploads/2017/05/riscv-spec-v2.2.pdf
+
+// RISC-V Specification, Table 19.1: RISC-V base opcode map.
+enum insn_op {
+    op_load = 0b00000,
+    op_load_fp = 0b00001,
+    op_custom_0 = 0b00010,
+    op_misc_mem = 0b00011,
+    op_imm = 0b00100,
+    op_auipc = 0b00101,
+    op_imm_32 = 0b00110,
+    op_store = 0b01000,
+    op_store_fp = 0b01001,
+    op_custom_1 = 0b01010,
+    op_amo = 0b01011,
+    op_op = 0b01100,
+    op_lui = 0b01101,
+    op_op_32 = 0b01110,
+    op_madd = 0b10000,
+    op_msub = 0b10001,
+    op_nmsub = 0b10010,
+    op_nmadd = 0b10011,
+    op_fp = 0b10100,
+    // reserved: 0b10100
+    op_custom2_rv128 = 0b10110,
+    op_branch = 0b11000,
+    op_jalr = 0b11001,
+    // reserved: 0b11010
+    op_system = 0b11100,
+    // reserved: 0b11101
+    op_custom3_rv128 = 0b11110,
 };
 
-enum r_funct3_discriminator {
-    FUNCT3_ADDI = 0b000,
-    FUNCT3_SLTI = 0b010,
-    FUNCT3_SLTIU = 0b011,
-    FUNCT3_XORI = 0b100,
-    FUNCT3_ORI = 0b110,
-    FUNCT3_ANDI = 0b111,
-    FUNCT3_SLLI = 0b001,
+// The following are set in the funct3 bitfield, or otherwise if specified.
+
+// Accompanied with an op_store opcode.
+// S-type.
+enum insn_store_func {
+    store_func_sb = 0b000,
+    store_func_sh = 0b001,
+    store_func_sw = 0b010,
 };
 
-
-union insn {
-    uint32_t raw;
-    struct {
-        enum insn_code code: 6;
-        union {
-            struct {
-                uint8_t rd: 5;
-                uint32_t imm: 20;
-            } u;
-            struct {
-                uint16_t imm: 12;
-                uint8_t rs1: 5;
-                enum r_funct3_discriminator funct3: 3;
-                uint8_t rd: 5;
-            } i;
-        };
-    };
+// Accompanied with an op_branch opcode.
+// B-type.
+enum insn_branch_func {
+    branch_func_beq = 0b000,
+    branch_func_bne = 0b001,
+    branch_func_blt = 0b100,
+    branch_func_bge = 0b101,
+    branch_func_bltu = 0b110,
+    branch_func_bgeu = 0b111,
 };
 
-#endif //SAM_INSN_H
+// Accompanied with an op_load opcode.
+// I-type.
+enum insn_load_func {
+    load_func_lb = 0b000,
+    load_func_lh = 0b001,
+    load_func_lw = 0b010,
+    load_func_lbu = 0b100,
+    load_func_lhu = 0b101,
+};
+
+// Accompanied with an op_imm opcode.
+// I-type.
+enum insn_imm_func {
+    imm_func_addi = 0b000,
+    imm_func_slti = 0b010,
+    imm_func_sltiu = 0b011,
+    imm_func_xori = 0b100,
+    imm_func_ori = 0b110,
+    imm_func_andi = 0b111,
+
+    // These used a specialized I-type format, in which the 5 low bits of the
+    // immediate is the shift amount, and the rest is fixed.
+    // The fixed values are in `enum shift_func`, and they're already
+    // shifted.
+
+    imm_func_slli = 0b001,
+    imm_func_srli = 0b101,
+    imm_func_srai = 0b101,
+};
+
+// These go in the upper 7 bits of he 12-bit immediate of the I-type
+// instruction.
+enum shift_func {
+    shift_func_srli = 0,
+    shift_func_srai = 1 << 5,
+};
+
+// Accompanied with an op_op opcode, and funct7 is `enum insn_op_funct7`
+// R-type.
+enum insn_op_funct3 {
+    op_funct3_add,
+    // sub is func7
+    op_funct3_sll,
+    op_funct3_slt,
+    op_funct3_sltu,
+    op_funct3_xor,
+    op_funct3_srl,
+    // sra is func7
+    op_funct3_or,
+    op_funct3_and,
+};
+
+enum insn_op_funct7 {
+    op_funct7_add = 0,
+    op_funct7_sub = 1 << 5,
+    op_funct7_sll = 0,
+    op_funct7_slt = 0,
+    op_funct7_sltu = 0,
+    op_funct7_xor = 0,
+    op_funct7_srl = 0,
+    op_funct7_sra = 1 << 5,
+    op_funct7_or = 0,
+    op_funct7_and = 0,
+};
+
+// CSR instructions. Accompanied with an op_system opcode.
+// I-type.
+enum insn_csr_funct3 {
+    csrrw,
+    csrrs,
+    csrrc,
+    csrrwi,
+    csrrsi,
+    csrrci,
+};
+
+enum csr_special_source {
+    rdcycle,
+    rdcycle_h,
+    rdtime,
+    rdtime_h,
+    rdinstret,
+    rdinstret_h,
+};
+
+enum insn_fence_funct3 {
+    // FENCE
+    fence_funct3 = 0,
+    // FENCE.I
+    fence_funct3_i = 1,
+
+};
+
+// ecall/ebreak is encoded via a I-type. Imm_11_0 is encoded by ecall_imm
+enum ecall_imm {
+    ecall_ecall = 0,
+    ecall_ebreak = 1 << 5,
+};
+
+enum fence_flags {
+    fence_i = 1 << 3,
+    fence_o = 1 << 2,
+    fence_r = 1 << 1,
+    fence_w = 1 << 0,
+};
+
+// These go in the func7 bitfield of the R-type instruction.
+
+// diff between U and J :
+// diff between S and B : 12-bit imm == multiples of 2 in B format.
+// The middle bits imm[10:1] stay fixed from the instruction, and the lowest bit
+// in S format (inst[7]) encodes a high-order bit in B format.
+
+struct insn {
+    union {
+        struct {
+            enum insn_op opcode : 7;
+            u32 unk_rest : 25;
+        } unknown;
+        // opcode is always op_misc_mem and funct3 is always FENCE.
+        // everything that is not
+        struct {
+            enum insn_op opcode : 7;
+            u8 rd : 5;
+            u8 funct3 : 3;
+            u8 rs1 : 3;
+            enum fence_flags successor : 4;
+            enum fence_flags predecessor : 4;
+            u8 always_zero : 4;
+        } __attribute__((packed)) fence;
+        struct {
+            enum insn_op opcode : 7;
+            u8 rd : 5;
+            u8 funct3 : 3;
+            u8 rs1 : 5;
+            u8 rs2 : 5;
+            u8 funct7 : 7;
+        } __attribute__((packed)) r;
+        struct {
+            enum insn_op opcode : 7;
+            u8 rd : 5;
+            u8 funct3 : 3;
+            u8 rs1 : 5;
+            u32 imm_11_0 : 12;
+        } __attribute__((packed)) i;
+        struct {
+            enum insn_op opcode : 7;
+            u8 imm_4_0 : 5;
+            u8 funct3 : 3;
+            u8 rs1 : 5;
+            u8 rs2 : 5;
+            u8 imm_11_5 : 7;
+        } __attribute__((packed)) s;
+        struct {
+            enum insn_op opcode : 7;
+            u8 imm_11 : 1;
+            u8 imm_4_1 : 4;
+            u8 funct3 : 3;
+            u8 rs1 : 5;
+            u8 rs2 : 5;
+            u8 imm_10_5 : 6;
+            u8 imm_12 : 1;
+        } __attribute__((packed)) b;
+        struct {
+            enum insn_op opcode : 7;
+            u8 rd : 5;
+            u32 imm_31_12 : 20;
+        } __attribute__((packed)) u;
+        struct {
+            enum insn_op opcode : 7;
+            u8 rd : 5;
+            u8 imm_19_12 : 8;
+            u8 imm_11 : 1;
+            u16 imm_10_1 : 10;
+            u8 imm_20 : 1;
+        } __attribute__((packed)) j;
+    } __attribute__((packed));
+} __attribute__((packed));
+
+// vim:ft=c
--- interpret.c
+++ interpret.c
@@ -34,76 +34,107 @@ void interpret(void const *memory, uint32_t entrypoint) {
 
     uint32_t pc = entrypoint;
     for (;; pc += 4) {
-        union insn insn;
+        union {
+            struct insn insn;
+            uint32_t raw;
+        } as;
         uint32_t const *insn_ptr = memory + pc;
         if (__builtin_expect((uintptr_t)insn_ptr & 0b11, 0)) {
             fputs("fatal: instructions MUST be aligned to 4 bytes\n", stderr);
             abort();
         }
-        insn.raw = *(uint32_t *)(__builtin_assume_aligned(insn_ptr, 4));
-        printf("insn: %x\n", insn.raw);
-        printf("opcode: %x\n", insn.code);
-        switch (insn.code) {
-        case INSN_LUI:
+        as.raw = *(uint32_t *)(__builtin_assume_aligned(insn_ptr, 4));
+        printf("insn: %x\n", as.raw);
+        printf("opcode: %x\n", as.insn.unknown.opcode);
+        switch (as.insn.unknown.opcode) {
+        case op_lui:
             // x[rd] = sext(immediate[31:12] << 12)
             // sext will be identity; since we're 32-bit.
-            cpu.registers[insn.u.rd] = read_upper_immediate(insn.raw);
+            cpu.registers[as.insn.u.rd] = read_upper_immediate(as.raw);
             break;
-        case INSN_AUIPC:
+        case op_auipc:
             // x[rd] = pc + sext(immediate[31:12] << 12)
-            cpu.registers[insn.u.rd] = pc + read_upper_immediate(insn.raw);
+            cpu.registers[as.insn.u.rd] = pc + read_upper_immediate(as.raw);
             break;
-        case INSN_ADDI_SLTI:
-            switch (insn.i.funct3) {
-            case FUNCT3_ADDI:
+        case op_imm:
+            switch ((enum insn_imm_func)as.insn.i.funct3) {
+            case imm_func_addi:
                 // x[rd] = x[rs1] + sext(immediate)
-                cpu.registers[insn.i.rd] =
-                    cpu.registers[insn.i.rs1] + sext32_imm12(insn.i.imm);
+                cpu.registers[as.insn.i.rd] = cpu.registers[as.insn.i.rs1] +
+                                              sext32_imm12(as.insn.i.imm_11_0);
                 break;
-            case FUNCT3_SLTI:
+            case imm_func_slti:
                 // x[rd] = x[rs1] <s sext(immediate)
                 {
                     int32_t signed_xrs1 =
-                        bit_cast_i32(cpu.registers[insn.i.rs1]);
-                    int32_t signed_imm = bit_cast_i32(sext32_imm12(insn.i.imm));
+                        bit_cast_i32(cpu.registers[as.insn.i.rs1]);
+                    int32_t signed_imm =
+                        bit_cast_i32(sext32_imm12(as.insn.i.imm_11_0));
                     // "cheat" by using an already implemented signed comparison
-                    cpu.registers[insn.i.rd] = signed_xrs1 < signed_imm;
+                    cpu.registers[as.insn.i.rd] = signed_xrs1 < signed_imm;
                 }
                 break;
-            case FUNCT3_SLTIU:
+            case imm_func_sltiu:
                 // x[rd] = x[rs1] <u sext(immediate)
                 // "cheat" by using an already implemented unsigned comparison
-                cpu.registers[insn.i.rd] =
-                    cpu.registers[insn.i.rs1] < sext32_imm12(insn.i.imm);
+                cpu.registers[as.insn.i.rd] = cpu.registers[as.insn.i.rs1] <
+                                              sext32_imm12(as.insn.i.imm_11_0);
                 break;
-            case FUNCT3_XORI: // sorry :(
+            case imm_func_xori: // sorry :(
                 // x[rd] = x[rs1] ^ sext(immediate)
-                cpu.registers[insn.i.rd] =
-                    cpu.registers[insn.i.rs1] ^ sext32_imm12(insn.i.imm);
+                cpu.registers[as.insn.i.rd] = cpu.registers[as.insn.i.rs1] ^
+                                              sext32_imm12(as.insn.i.imm_11_0);
                 break;
-            case FUNCT3_ORI:
+            case imm_func_ori:
                 // x[rd] = x[rs1] | sext(immediate)
-                cpu.registers[insn.i.rd] =
-                    cpu.registers[insn.i.rs1] | sext32_imm12(insn.i.imm);
+                cpu.registers[as.insn.i.rd] = cpu.registers[as.insn.i.rs1] |
+                                              sext32_imm12(as.insn.i.imm_11_0);
                 break;
-            case FUNCT3_ANDI:
+            case imm_func_andi:
                 // x[rd] = x[rs1] & sext(immediate)
-                cpu.registers[insn.i.rd] =
-                    cpu.registers[insn.i.rs1] & sext32_imm12(insn.i.imm);
+                cpu.registers[as.insn.i.rd] = cpu.registers[as.insn.i.rs1] &
+                                              sext32_imm12(as.insn.i.imm_11_0);
                 break;
-            case FUNCT3_SLLI:
+            case imm_func_slli:
                 // x[rd] = x[rs1] << shamt
                 // shamt is lower five bits, since anything else would wrap
                 // around.
-                cpu.registers[insn.i.rd] = cpu.registers[insn.i.rs1]
-                                           << (insn.i.imm & 0x1f);
+                cpu.registers[as.insn.i.rd] = cpu.registers[as.insn.i.rs1]
+                                              << (as.insn.i.imm_11_0 & 0x1f);
                 break;
-            default:
-                assert(!"Unknown funct3 code");
+            case imm_func_srli: {
+                u8 shift_count = as.insn.i.imm_11_0 & ((1 << 5) - 1);
+                u32 rest = as.insn.i.imm_11_0 & ~((1 << 5) - 1);
+                switch ((enum shift_func)rest) {
+                case shift_func_srli:
+                case shift_func_srai:
+                    assert(!"not implemented");
+                }
+            }
             }
             break;
-        default:
-            assert(!"Unknown insn opcode");
+        case op_load:
+        case op_load_fp:
+        case op_custom_0:
+        case op_misc_mem:
+        case op_imm_32:
+        case op_store:
+        case op_store_fp:
+        case op_custom_1:
+        case op_amo:
+        case op_op:
+        case op_op_32:
+        case op_madd:
+        case op_msub:
+        case op_nmsub:
+        case op_nmadd:
+        case op_fp:
+        case op_custom2_rv128:
+        case op_branch:
+        case op_jalr:
+        case op_system:
+        case op_custom3_rv128:
+            assert(!"not implemented");
         }
     }
 }
